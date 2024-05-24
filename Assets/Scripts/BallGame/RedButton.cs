2359b0b035ca1da66e4a17be2aa246cec489ac91
Delmana <35628648+Delmana@users.noreply.github.com>
2024-05-24 11:32:14 +0200

    RedButton

--- Assets/Scripts/BallGame/RedButton.cs
+++ Assets/Scripts/BallGame/RedButton.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class RedButton : MonoBehaviour
 {
-  
+    public OVRInput.Controller controller;
+    private float triggerValue;
+    private bool isInCollider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller);
+
+        if (isInCollider && other.gameObject.CompareTag("RedButton"))
+        {
+            if (triggerValue > 0.95f )
+            {
+                GameManager.Instance.StartGame();
+                Destroy(other.gameObject);
+            }
+
+        }
+    }
+
+    
 }
