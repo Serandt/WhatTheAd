3a6a0c7aee2b6ab93b22351bfe680e091e1068ad
Delmana <35628648+Delmana@users.noreply.github.com>
2024-06-12 10:38:04 +0200

    bin

--- Assets/Scripts/Dark Patterns/Bin.cs
+++ Assets/Scripts/Dark Patterns/Bin.cs
@@ -7,7 +7,7 @@ public class Bin : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
        
-        if (other.CompareTag("FalsBall"))
+        if (other.CompareTag("FalseBall"))
         {
             DarkPatternManager.Instance.removeDarkPatternFromList(DarkPatternManager.Instance.activePatterns.Find(item => item.ID == other.GetComponent<FalseBall>().id).gameObject);
             Destroy(this.gameObject);
