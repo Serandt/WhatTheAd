55ee122b726d263921bcc7b163fb4b6874e2ea29
Serandt <36221849+Serandt@users.noreply.github.com>
2024-06-17 13:48:33 +0200

    bug fix: false ball

--- Assets/Scripts/Dark Patterns/FalseBall.cs
+++ Assets/Scripts/Dark Patterns/FalseBall.cs
@@ -6,4 +6,12 @@ public class FalseBall : MonoBehaviour
 {
     public GameObject obj;
     public Vector3 spawnPosition;
+
+    private void Update()
+    {
+        if (this.transform.position.y < -0.2f)
+        {
+            transform.position = spawnPosition;
+        }
+    }
 }
