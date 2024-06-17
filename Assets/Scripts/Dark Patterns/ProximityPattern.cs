cb9334a0db621a659e98f94d074920d7a030ffc8
Delmana <35628648+Delmana@users.noreply.github.com>
2024-06-17 11:02:08 +0200

    proximity Ad

--- Assets/Scripts/Dark Patterns/ProximityPattern.cs
+++ Assets/Scripts/Dark Patterns/ProximityPattern.cs
@@ -5,41 +5,94 @@ using UnityEngine.AI;
 
 public class ProximityPattern : DarkPattern
 {
-    public float fleeDistance = 0.5f; 
-    
-    public NavMeshAgent agent; 
-  
-    public float fleeMultiplier = 0.1f;
+    /* public float fleeDistance = 0.5f; 
 
+     public NavMeshAgent agent; 
+
+     public float fleeMultiplier = 0.1f;
+
+     private void Start()
+     {
+         agent = GetComponent<NavMeshAgent>();
+     }
+
+     void Update()
+     {
+         float distance = Vector3.Distance(transform.position, DarkPatternManager.Instance.GetCameraPos());
+
+         if (distance < fleeDistance)
+         {
+             Vector3 directionToPlayer = transform.position - DarkPatternManager.Instance.GetCameraPos();
+             agent.SetDestination(directionToPlayer);
+         }
+
+
+
+       /*  if (distance < fleeDistance)
+         {
+             Vector3 directionToPlayer = transform.position - DarkPatternManager.Instance.GetCameraPos();
+             directionToPlayer.Normalize();  
+             Vector3 fleeDestination = transform.position + directionToPlayer * fleeDistance;  
+
+
+             if (NavMesh.SamplePosition(fleeDestination, out NavMeshHit hit, fleeDistance, NavMesh.AllAreas))
+             {
+                 agent.SetDestination(hit.position);
+             }
+         }*/
+
+
+    public float fleeDistance = 1f;
+    public NavMeshAgent agent;
+    public float checkDistance = 2f;
+    
     private void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+    agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        float distance = Vector3.Distance(transform.position, DarkPatternManager.Instance.GetCameraPos());
-        
+        Vector3 playerPosition = DarkPatternManager.Instance.GetCameraPos();
+        float distance = Vector3.Distance(transform.position, playerPosition);
+
         if (distance < fleeDistance)
         {
-            Vector3 directionToPlayer = transform.position - DarkPatternManager.Instance.GetCameraPos();
-            agent.SetDestination(directionToPlayer);
-        }
+            Vector3 directionToPlayer = transform.position - playerPosition;
+            directionToPlayer.Normalize();
+            Vector3 potentialDestination = transform.position + directionToPlayer * fleeDistance;
 
-   
-
-      /*  if (distance < fleeDistance)
-        {
-            Vector3 directionToPlayer = transform.position - DarkPatternManager.Instance.GetCameraPos();
-            directionToPlayer.Normalize();  
-            Vector3 fleeDestination = transform.position + directionToPlayer * fleeDistance;  
-
-            
-            if (NavMesh.SamplePosition(fleeDestination, out NavMeshHit hit, fleeDistance, NavMesh.AllAreas))
+            // Überprüfen, ob der Fluchtpunkt gültig ist
+            if (NavMesh.SamplePosition(potentialDestination, out NavMeshHit hit, checkDistance, NavMesh.AllAreas))
             {
                 agent.SetDestination(hit.position);
             }
-        }*/
+            else
+            {
+                // Finden Sie eine alternative Route entlang des Randes
+                FindPathAlongEdge(directionToPlayer);
+            }
+        }
+    }
+
+    void FindPathAlongEdge(Vector3 directionToPlayer)
+    {
+    // Probieren Sie, nach links oder rechts auszuweichen
+    Vector3 sideStep = Quaternion.Euler(0, 90, 0) * directionToPlayer * fleeDistance;
+    if (!NavMesh.SamplePosition(transform.position + sideStep, out NavMeshHit hitLeft, checkDistance, NavMesh.AllAreas))
+    {
+        sideStep = Quaternion.Euler(0, -90, 0) * directionToPlayer * fleeDistance;
+        if (NavMesh.SamplePosition(transform.position + sideStep, out NavMeshHit hitRight, checkDistance, NavMesh.AllAreas))
+        {
+            agent.SetDestination(hitRight.position);
+        }
+        // Keine gültigen Punkte gefunden: Letzter Versuch könnte ein über den Spieler springen sein
+        // (benötigt Erweiterungen des NavMesh und des Agenten)
+    }
+    else
+    {
+        agent.SetDestination(hitLeft.position);
+    }
     }
 }
 
