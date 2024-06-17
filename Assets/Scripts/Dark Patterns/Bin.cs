083f1488e484faa78b58e9bf398cc4ffcfd64619
Serandt <36221849+Serandt@users.noreply.github.com>
2024-06-17 10:15:34 +0200

    size matters

--- Assets/Scripts/Dark Patterns/Bin.cs
+++ Assets/Scripts/Dark Patterns/Bin.cs
@@ -1,19 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bin : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-       
         if (other.CompareTag("FalseBall"))
         {
-
-            //DarkPatternManager.Instance.removeDarkPatternFromList(DarkPatternManager.Instance.activePatterns.Find(item => item.ID == other.GetComponent<FalseBall>().id).GameObject);
             DarkPatternManager.Instance.removeDarkPatternFromList(other.GetComponent<FalseBall>().obj);
-            Destroy(this.gameObject);
+            ControllerInteraction.Instance.ControllerVibration();
             Destroy(other.gameObject);
+            Destroy(this.gameObject);
         }
     }
 }
--- Assets/Scripts/VR/ControllerInteraction.cs
+++ Assets/Scripts/VR/ControllerInteraction.cs
@@ -4,6 +4,13 @@ public class ControllerInteraction : MonoBehaviour
 {
     private GameManager.Condition cond;
 
+    public static ControllerInteraction Instance;
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("RedButton"))
@@ -22,6 +29,17 @@ public class ControllerInteraction : MonoBehaviour
         if (other.CompareTag("ProximityAd")|| other.CompareTag("Popup") || other.CompareTag("TutorialAd")) 
         {
             DarkPatternManager.Instance.removeDarkPatternFromList(other.gameObject);
+            ControllerVibration();
         }
+    }
+    public void ControllerVibration()
+    {
+        OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
+        Invoke("StopControllerVibration", 0.2f);
+    }
+
+    void StopControllerVibration()
+    {
+        OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
     }
 }
